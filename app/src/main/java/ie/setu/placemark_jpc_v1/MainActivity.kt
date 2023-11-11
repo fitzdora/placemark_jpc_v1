320b51c2bf1a69daf9fed815e87afe7fd3737375
fitzdora <33418972+fitzdora@users.noreply.github.com>
2023-11-11 09:29:32 +0000

    Adding Toolbar and addPlacemark

--- app/src/main/java/ie/setu/placemark_jpc_v1/MainActivity.kt
+++ app/src/main/java/ie/setu/placemark_jpc_v1/MainActivity.kt
@@ -3,13 +3,40 @@ package ie.setu.placemark_jpc_v1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ie.setu.placemark_jpc_v1.ui.theme.PlacemarkJPCV1Theme
 import timber.log.Timber
 import timber.log.Timber.Forest.i
@@ -25,7 +52,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(modifier = Modifier.fillMaxSize())
+                    {
+                        ShowToolBar()
+                        ShowAddPlacemark()
+                    }
                 }
             }
         }
@@ -47,5 +78,72 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     PlacemarkJPCV1Theme {
         Greeting("Android")
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun ShowAddPlacemark() {
+    var title by remember { mutableStateOf("") }
+
+    PlacemarkJPCV1Theme {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(all = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = MaterialTheme.colorScheme.primary,
+                    unfocusedBorderColor = MaterialTheme.colorScheme.primary,
+                    cursorColor = MaterialTheme.colorScheme.primary,
+                    placeholderColor = MaterialTheme.colorScheme.onPrimary
+                ),
+                value = title,
+                onValueChange = { title = it },
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(stringResource(id = R.string.text_titleHint)) },
+                trailingIcon = {
+                    Icon(
+                        Icons.Default.Edit, contentDescription = "",
+                        tint = Color.Black
+                    )
+                },
+            )
+            Button(
+                onClick = { },
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                elevation = ButtonDefaults.buttonElevation(20.dp)
+            ) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
+                Spacer(modifier = Modifier.width(width = 4.dp))
+                Text(stringResource(id = R.string.button_addPlacemark))
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun ShowToolBar(){
+    PlacemarkJPCV1Theme {
+        TopAppBar(
+            title = {
+                Text(text = stringResource(id = R.string.app_name),
+                color = Color.White)
+            },
+            colors = TopAppBarDefaults.largeTopAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primary
+            ),
+            navigationIcon = {
+                Icon(imageVector = Icons.Filled.Menu,
+                    contentDescription = "Menu",
+                    tint = Color.White)
+            }
+        )
     }
 }
